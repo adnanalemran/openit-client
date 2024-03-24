e7fe87996dd8b0a15dffb7fe67c261fd4ee3527e
adnanalemran <adnanalemranontor@gmail.com>
2024-03-24 14:10:23 +0600

    update faculty

--- src/Pages/HomePage/Faculty/Faculty.jsx
+++ src/Pages/HomePage/Faculty/Faculty.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { FaPhone, FaUser } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
+import { Link } from "react-router-dom";
 const Faculty = () => {
   const [faculty, setFaculty] = useState([]);
 
@@ -12,7 +13,7 @@ const Faculty = () => {
 
   return (
     <div className="py-8 text-bangla   ">
-       <div className="text-center ">
+      <div className="text-center ">
         <h2 className="text-3xl font-semibold text-gray-950 pb-3">
           Our Mentor
         </h2>
@@ -21,43 +22,59 @@ const Faculty = () => {
         </p>
       </div>
 
-     
-
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-8 shadow  mt-4 p-4 ">
+      <div className="lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 w-full py-12 ">
         {faculty.map((singlefaculty) => (
-          <div
-            key={singlefaculty.id}
-            className="card card-compact bg-base-300 p-6 hover:shadow-lg transition duration-300    hover:scale-105 shadow-xl"
-          >
-            <div className="avatar mt-4">
-              <div className="w-44 mx-auto rounded-full">
-                <img src={singlefaculty.photoUrl} />
-              </div>
-            </div>
-            <div className="card-body ">
-              <h2 className="card-title">
-                <FaUser size={15} /> {singlefaculty?.name}
-              </h2>
-              <p>Designation: {singlefaculty.designation} </p>
-              <div className="card-actions justify-end">
-                <p className="flex gap-2">
-                  <FaPhone size={16} />
-                  <a href={`tel:${singlefaculty?.mobileNo}`}>
-                    {singlefaculty?.mobileNo}
-                  </a>
-                </p>
-                <p className="flex gap-2">
-                  <FiMail size={20} />
-                  <a href={`mailto:${singlefaculty?.email}`}>
-                    {singlefaculty?.email}
-                  </a>
-                </p>
+          <div key={singlefaculty.id} className="card      p-2     ">
+            <div className="card w-full   shadow-xl relative hover:shadow-2xl mx-auto ">
+              <a title="click for product detail page">
+                <figure className=" px-2">
+                  <img
+                    src={singlefaculty.photoUrl}
+                    alt="man"
+                    className="rounded-xl my-2 w-[390px] h-[270px] object-cover  "
+                  />
+                </figure>
+              </a>
+              <div className=" pb-5">
+                <div className="  ">
+                  <div className="flex justify-between p-4">
+                    <div className="">
+                      <h2 className="   font-bold  text-xl  text-gray-900  ">
+                        {singlefaculty?.name}
+                      </h2>
+                    </div>
+
+                    <div className=""></div>
+                  </div>
+                  <div className="flex justify-between text-gray-600 text-xs p-4 ">
+                    <div className="">{singlefaculty?.profession}</div>
+                  </div>
+
+                  <div className="text-gray-600 text-xs px-4  ">
+                    {singlefaculty?.about}
+                  </div>
+
+                  <div className="text-orange-500  text-xs p-4">
+                    {singlefaculty?.designation}
+                  </div>
+
+                  <div className="card-actions justify-end px-4   text-gray-600 text-xs ">
+                   
+                    <p className="flex gap-2 items-center">
+                      <FiMail size={20} />
+                      <a href={`mailto:${singlefaculty?.email}`}>
+                        {singlefaculty?.email}
+                      </a>
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         ))}
       </div>
+
+      <div className="lg:flex gap-6 w-full py-12   "></div>
     </div>
   );
 };
