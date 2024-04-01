f04d45c893a99f8d5422c440a4e1888202f9a146
adnanalemran <adnanalemranontor@gmail.com>
2024-04-02 04:47:08 +0600

    removed unused file

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,16 +1,15 @@
-import React from "react";
-import Slider from "./Slider/Slider";
+
 import HomeInfo from "./HomeInfo/HomeInfo";
 import Faculty from "./Faculty/Faculty";
-import CumpusLife from "./CumpusLife/CumpusLife";
+
 import OurAlumni from "./OurAlumni/OurAlumni";
 
 import Subscribe from "../../components/Homepage/Subscribe";
 import Featured from "../../components/Homepage/Featured";
-import DealsAndOffers from "../../components/Homepage/DealsAndOffers";
+ 
 import HeroSection from "../../components/Homepage/HeroSection";
 import Premium from "../../components/Homepage/Premium";
-import CumpusLifesmall from "./CumpusLife/CumpusLifesmall";
+
 import Book from "../../components/Homepage/book";
 import Patnar from "../../components/Homepage/Patnar";
 
@@ -18,7 +17,7 @@ const HomePage = () => {
   return (
     <div className="">
    
-      {/* <Slider /> */}
+ 
       <HeroSection />
 
       <div className="   mx-auto overflow-hidden ">
