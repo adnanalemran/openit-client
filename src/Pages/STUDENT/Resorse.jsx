3d658232302cc90a5978e82f2b0451b5ea2ad564
adnanalemran <adnanalemranontor@gmail.com>
2024-04-01 23:35:54 +0600

    update resorse

--- src/Pages/STUDENT/Resorse.jsx
+++ src/Pages/STUDENT/Resorse.jsx
@@ -1,10 +1,27 @@
-import ComeInSoon from "../../components/Shared/Header/ComeInSoon";
-
+import Iframe from "react-iframe";
  
 const Resorse = () => {
     return (
         <div className="min-h-screen">
-           <ComeInSoon/>
+           
+           <Iframe
+        url="https://file.openit-edu.com/"
+        width="640px"
+        height="320px"
+        id=""
+        className="w-full h-[600px] my-8 rounded-sm"
+        display="block"
+        position="relative"
+      />
+
+
+
+
+
+
+
+
+
         </div>
     );
 };
--- src/Pages/dashboard/Student/StudentMenu.jsx
+++ src/Pages/dashboard/Student/StudentMenu.jsx
@@ -17,7 +17,7 @@ const StudentMenu = () => {
           <FaCoffee className="mx-4" />
           <p className="font-medium text-lg">Resource </p>
         </li>
-      </NavLink>{" "}
+      </NavLink> 
       <NavLink to="/dashboard/TypingTest">
         <li className="flex p-2 gap-1 bg-gradient-to-r from-[#ba97e9] to-[#9756F5] text-white w-full items-center rounded-r-3xl hover:from-[#ac8cd6] hover:to-[#9756F5] transition-all duration-500">
           <SiSpeedtest className="mx-4" />
